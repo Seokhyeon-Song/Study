33debfe8d56b1832835705422ecddb4f2e970761
Seokhyeon-Song <hypercube256@snu.ac.kr>
2024-10-15 13:44:26 +0900

    two loops

---
+++ FeynRules/Models/Massless 3scalar/ReductionTable.m
@@ -0,0 +1,10 @@
+FIREImportResults[{FCTopology["fctopology1", 
+   {FeynAmpDenominator[StandardPropagatorDenominator[Momentum[r, D], 0, 0, 
+      {1, 1}]], FeynAmpDenominator[StandardPropagatorDenominator[
+      Momentum[q, D], 0, 0, {1, 1}]], FeynAmpDenominator[
+     StandardPropagatorDenominator[Momentum[p + q, D], 0, 0, {1, 1}]], 
+    FeynAmpDenominator[StandardPropagatorDenominator[Momentum[p - r, D], 0, 
+      0, {1, 1}]], FeynAmpDenominator[StandardPropagatorDenominator[
+      Momentum[q + r, D], 0, -m^2, {1, 1}]]}, {q, r}, {p}, 
+   {Hold[Pair][Momentum[p, D], Momentum[p, D]] -> s}, {}]}, 
+ "C:\\Users\\HEP-ph\\Desktop\\Study\\FeynRules\\Models\\Massless 3scalar\\"]
